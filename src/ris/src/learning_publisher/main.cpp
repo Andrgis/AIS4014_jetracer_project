e6ebed8a02680a29e06d52bdbdafa46b3227c0a8
Andrgis <andrgis@stud.ntnu.no>
2025-03-29 11:52:21 +0100

    Made a message for dog_data and updated learning_nodes to publish the data and subscribe to data.

--- src/ris/src/learning_publisher/main.cpp
+++ src/ris/src/learning_publisher/main.cpp
@@ -1,19 +1,20 @@
 #include <ros/ros.h> // Ros library
-#include <std_msgs/String.h> // For writing a standard string msg
-
+#include <ris_msgs/Dog_data.h> // For writing a standard string msg
 
 int main(int argc, char **argv){
   ros::init(argc, argv, "Publisher"); // Initiating ros node
   ros::NodeHandle node; // Declaring the node handle
 
   // Declaring the publisher node using the node handle. Topic name: "learning_publiser". Max number of msg: 100.
-  ros::Publisher topic_publisher = node.advertise<std_msgs::String>("learning_publisher", 100);
-  ros::Rate loop_rate(1); // setting how often the publisher should publish
+  ros::Publisher topic_publisher = node.advertise<ris_msgs::Dog_data>("learning_topic", 100);
+  ros::Rate loop_rate(2); // setting how often the publisher should publish
 
   while(ros::ok()){
-    std_msgs::String msg; // Declaring message
-    msg.data = "Andreas is the ROS champ!"; // Setting value for the message
-    topic_publisher.publish(msg); // Publishing message to topic
+    ris_msgs::Dog_data dog; // Declaring message
+    dog.name = "Rocky"; // Setting value for the message
+    dog.breed = "Bishon Havanais";
+    dog.age = 12;
+    topic_publisher.publish(dog); // Publishing message to topic
     ros::spinOnce(); // Ros spins ???
     loop_rate.sleep(); // Sleeps for 1 second before looping again
   }
--- src/ris/src/learning_subscriber/main.cpp
+++ src/ris/src/learning_subscriber/main.cpp
@@ -1,15 +1,17 @@
 #include <ros/ros.h> // Ros library
-#include <std_msgs/String.h> // For writing a standard string msg
+#include <ris_msgs/Dog_data.h> // For writing a standard string msg
 
-void writeMsgToLog(const std_msgs::String::ConstPtr& msg){
-  ROS_INFO("We have recieved and confirmed that: '%s'", msg->data.c_str());
+void writeMsgToLog(const ris_msgs::Dog_data &dog_data){
+  ROS_INFO("The ros dog's name is: %s", dog_data.name.c_str());
+  ROS_INFO("The ros dog's breed is: '%s'", dog_data.breed.c_str());
+  ROS_INFO("The ros dog's age is: %i years", dog_data.age);
 }
 
 int main(int argc, char **argv){
   ros::init(argc, argv, "Subscriber"); // Initiating ros node
   ros::NodeHandle node; // Declaring the node handle
 
-  ros::Subscriber topic_subscriber = node.subscribe("learning_publisher", 100, writeMsgToLog);
+  ros::Subscriber topic_subscriber = node.subscribe("learning_topic", 100, writeMsgToLog);
 
   ros::spin(); // Checks for update?
 }
